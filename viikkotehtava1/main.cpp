b974d4608d76c17f0f93a136aac3ee955f3dcdc6
Reko Tornberg <rekotornberg@tlook.com>
2025-01-07 17:11:47 +0200

    Viikkotehtava 1

---
+++ viikkotehtava1/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int game(int maxnum);
+
+int main()
+{
+    int maxnum = 40;
+    int arvaukset = game(maxnum);
+    cout <<"Arvausten lukumaara: " << arvaukset << endl;
+    return 0;
+}
+
+int game(int maxnum){
+    int num = 0;
+    int satNum;
+    int arvaukset = 1;
+
+    srand(time(NULL));
+    satNum = rand() % maxnum + 1;
+    //cout << satNum << endl;
+    while (true){
+        cout << "Arvaa numero 1-" << maxnum << endl;
+        cin >> num;
+        if (satNum == num) {
+            cout << "Oikein!" << endl;
+            break;
+        } else if (satNum < num) {
+            cout << "Arvottu numero on pienempi kuin " << num << endl;
+            arvaukset += 1;
+
+        } else if (satNum > num) {
+            cout << "Arvottu numero on suurempi kuin " << num << endl;
+            arvaukset += 1;
+        }
+    }
+    return arvaukset;
+}
+
+
